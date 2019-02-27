0dd0b269accdd814f7b73e44040da0ca308fd04f
Paulo Marcos <paulo.marcos@moxy.studio>
2019-02-27 17:14:08 +0000

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+class PicoSignals {
+    listeners = new Set();
+
+    add(listener) {
+        this.listeners.add(listener);
+
+        return () => this.delete(listener);
+    }
+
+    delete(listener) {
+        return this.listeners.delete(listener);
+    }
+
+    clear() {
+        this.listeners.clear();
+    }
+
+    dispatch(...args) {
+        this.listeners.forEach((listener) => listener(...args));
+    }
+}
+
+const createPicoSignals = () => new PicoSignals();
+
+export default createPicoSignals;
---
+++ tests/index.test.js
@@ -0,0 +1,66 @@
+import picoSignals from '../index';
+
+it('should have all methods', () => {
+    const instance = picoSignals();
+
+    expect(typeof instance.add).toEqual('function');
+    expect(typeof instance.delete).toEqual('function');
+    expect(typeof instance.clear).toEqual('function');
+    expect(typeof instance.dispatch).toEqual('function');
+});
+
+it('should add listener and provide method to delete it', () => {
+    const instance = picoSignals();
+    const mockListener = jest.fn();
+
+    const deleteListener = instance.add(mockListener);
+
+    expect(typeof deleteListener).toEqual('function');
+    expect(instance.listeners.size).toEqual(1);
+
+    deleteListener();
+
+    expect(instance.listeners.size).toEqual(0);
+});
+
+it('should delete listener', () => {
+    const instance = picoSignals();
+    const mockListener = jest.fn();
+
+    instance.add(mockListener);
+
+    expect(instance.listeners.size).toEqual(1);
+
+    instance.delete(mockListener);
+
+    expect(instance.listeners.size).toEqual(0);
+});
+
+it('should clear all listeners', () => {
+    const instance = picoSignals();
+
+    instance.add(() => jest.fn());
+    instance.add(() => jest.fn());
+
+    expect(instance.listeners.size).toEqual(2);
+
+    instance.clear();
+
+    expect(instance.listeners.size).toEqual(0);
+});
+
+it('should call every listener on dispatch', () => {
+    const instance = picoSignals();
+    const mockListener1 = jest.fn();
+    const mockListener2 = jest.fn();
+
+    instance.add(mockListener1);
+    instance.add(mockListener2);
+    instance.dispatch('arg1', 'arg2');
+
+    expect(mockListener1).toHaveBeenCalledTimes(1);
+    expect(mockListener1).toHaveBeenCalledWith('arg1', 'arg2');
+
+    expect(mockListener2).toHaveBeenCalledTimes(1);
+    expect(mockListener2).toHaveBeenCalledWith('arg1', 'arg2');
+});
